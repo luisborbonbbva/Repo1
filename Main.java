f074bc5c80e9cacaa5ad2314e8c7d42b07e0374c
Hernan Dario Ramos Ortiz <hdramos@efron.loc>
2020-03-12 11:40:00 -0500

    MetodoDiez

--- Main.java
+++ Main.java
@@ -21,4 +21,8 @@ public class main{
 	public void saludoRamaDiez(){
 		System.out.println("Metodo de la rama 10");
 	}
+	
+	public void saludoRomaOnce(){
+		System.out.println("Metodo de la rama 11");
+	}
 }
