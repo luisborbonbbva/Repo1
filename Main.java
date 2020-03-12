40e8f8519f6305bd0fa9280e1aa05ba480dd2075
Hernan Dario Ramos Ortiz <hdramos@efron.loc>
2020-03-12 11:01:52 -0500

    last

--- Main.java
+++ Main.java
@@ -1,22 +1,18 @@
 public class main{
 	public static void main(String[] args){
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
 	public void saludoRamaCinco(){
 		System.out.println("Metodo de la rama 5");
-=======
+	}
 	
 	public void saludoRamaCuatro(){
 		System.out.println("Metodo de la rama 4");
->>>>>>> RamaCuatro
-=======
+	}
 		public void saludoRamaDos(){
 		System.out.println("Metodo de la rama dos");
-=======
+		}
 		public void saludoRamaTres(){
 		System.out.println("Metodo de la rama 3");
->>>>>>> RamaTres
->>>>>>> RamaSeis
+		}
 	}
 }
